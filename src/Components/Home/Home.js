f8e32b481c846371d558c1730de1b03a8b514a57
Nazmush Shakib <shakibgithub944>
2023-01-06 13:30:33 +0600

    App initialise

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navber from '../Navber/Navber';
+import Newses from '../NewsFeed/Newses';
+
+const Home = () => {
+    return (
+        <div>
+            <Navber/>
+            <Newses/>
+        </div>
+    );
+};
+
+export default Home;
